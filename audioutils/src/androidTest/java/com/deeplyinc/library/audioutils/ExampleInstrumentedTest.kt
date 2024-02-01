dc88e366c5945a813366db00dcfa3e24c17cc258
KimGyeongyeon <kgy556@naver.com>
2024-02-01 12:28:53 +0900

    feat: upgrade AGP 7.1.3 -> 8.2.2 & Kotlin 1.9.20

--- audioutils/src/androidTest/java/com/deeplyinc/library/audioutils/ExampleInstrumentedTest.kt
+++ audioutils/src/androidTest/java/com/deeplyinc/library/audioutils/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.deeplyinc.library
+package com.deeplyinc.library.audioutils
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
